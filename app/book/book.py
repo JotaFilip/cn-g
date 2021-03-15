04d07125ed5e112f3298506106175bd7590cb013
João Afonso <johnlandon1999@gmail.com>
2021-03-15 20:04:23 +0000

    Testar com mock de book.py

--- app/book/book.py
+++ app/book/book.py
@@ -6,6 +6,7 @@ from grpc_interceptor import ExceptionToStatusInterceptor
 from grpc_interceptor.exceptions import NotFound
 
 from book_pb2 import (
+    BookCategory,
     BookData,
     BookDataList,
     BookByIdRequest,
@@ -17,22 +18,32 @@ import book_pb2_grpc
 
 # Acesso ah base de dados
 
+books_database = {
+    "id": ["1", "2", "3", "4", "5"]
+    "name": ["a", "b", "c", "d", "e"]
+    "genre": ["luta", "acao", "romance", "luta", "acao"]
+}
+
 
 class BookService(book_pb2_grpc.BookServicer):
     def SearchById(self, request, context):
         
-        if request.book_id not in books_database:
+        #if request.book_id not in books_database.id:
+        if request.book_id not in books_database["id"]:
             raise NotFound("Id not found")
 
-        book_with_id = books_database[request.book_id]
+        #book_with_id = books_database[request.book_id]
+        book_with_id = books_database["id"][request.book_id]
         return BookData(books=book_with_id)
         
     def SearchByName(self, request, context):
     
-        if request.name not in books_database:
+        #if request.name not in books_database:
+        if request.book_id not in books_database["name"]:
             raise NotFound("Name not found")
         
-        books_with_name = books_database[request.name]
+        #books_with_name = books_database[request.name]
+        book_with_id = books_database["name"][request.name]
         num_results = min(request.max_results, len(books_with_name))
         searched_books = random.sample(books_with_name, num_results)
 
@@ -40,10 +51,13 @@ class BookService(book_pb2_grpc.BookServicer):
         
     def SearchByCategory(self, request, context):
     
-        if request.category not in books_database:
+        #if request.category not in books_database:
+        
+        if request.book_id not in books_database["genre"]:
             raise NotFound("Category not found")
         
-        books_with_category = books_database[request.category]
+        #books_with_category = books_database[request.category]
+        book_with_id = books_database["genre"][request.category]
         num_results = min(request.max_results, len(books_with_name))
         searched_books = random.sample(books_with_category, num_results)
 
