f89ab96fa547c8d6402a032386c0c92436a69faf
João Afonso <johnlandon1999@gmail.com>
2021-03-15 13:01:21 +0000

    versao de book.py que ainda nao tem acesso ah base de dados

--- app/book/book.py
+++ app/book/book.py
@@ -1,81 +1,53 @@
 from concurrent import futures
+from pymongo import MongoClient
 
 import grpc
 from grpc_interceptor import ExceptionToStatusInterceptor
 from grpc_interceptor.exceptions import NotFound
 
 from recommendationsBook_pb2 import (
-    RecommendationBookRequest,
-    BookRecommendation,
-    RecommendationBookResponse,
-    BookNameRequest,
-    BookCategoryRequest,
+    BookData,
+    BookDataList,
+    BookByIdRequest,
+    BooksByNameRequest,
+    BooksByCategoryRequest,
     
 )
 import recommendationsBook_pb2_grpc
-
-books_by_category = {
-    BookCategory.MYSTERY: [
-        BookRecommendation(id=1, title="The Maltese Falcon"),
-        BookRecommendation(id=2, title="Murder on the Orient Express"),
-        BookRecommendation(id=3, title="The Hound of the Baskervilles"),
-    ],
-    BookCategory.SCIENCE_FICTION: [
-        BookRecommendation(id=4, title="The Hitchhiker's Guide To The Galaxy"),
-        BookRecommendation(id=5, title="Ender's Game"),
-        BookRecommendation(id=6, title="The Dune Chronicles"),
-    ],
-    BookCategory.SELF_HELP: [
-        BookRecommendation(
-            id=7, title="The 7 Habits of Highly Effective People"
-        ),
-        BookRecommendation(
-            id=8, title="How to Win Friends and Influence People"
-        ),
-        BookRecommendation(id=9, title="Man’s Search for Meaning"),
-    ],
-}
 
 # Acesso ah base de dados
 
 
 class BookService(recommendationsBook_pb2_grpc.RecommendationsBookServicer):
-    def RecommendBook(self, request, context):
+    def SearchById(self, request, context):
         
-        books_to_recommend_by_criteria = {}
-        
-        for x in request.criteria:
-            books_with_criteria = books_database[x]
-            num_results = min(request.max_results, len(books_to_recommend))
-            books_to_recommend = random.sample(books_with_criteria, num_results)
-            books_to_recommend_by_criteria[x] = books_to_recommend
+        if request.book_id not in books_database:
+            raise NotFound("Id not found")
 
-        return RecommendationResponse(recommendations=books_to_recommend_by_criteria)
+        book_with_id = books_database[request.book_id]
+        return RecommendationResponse(books=book_with_id)
         
     def SearchByName(self, request, context):
-        if request.category not in books_by_category:
-            raise NotFound("Category not found")
+    
+        if request.name not in books_database:
+            raise NotFound("Name not found")
+        
+        books_with_name = books_database[request.name]
+        num_results = min(request.max_results, len(books_with_name))
+        searched_books = random.sample(books_with_name, num_results)
 
-        books_for_category = books_by_category[request.category]
-        num_results = min(request.max_results, len(books_for_category))
-        books_to_recommend = random.sample(books_for_category, num_results)
-
-        return RecommendationResponse(recommendations=books_to_recommend)
+        return RecommendationResponse(books=searched_books)
         
     def SearchByCategory(self, request, context):
-        if request.category not in books_by_category:
+    
+        if request.category not in books_database:
             raise NotFound("Category not found")
-
-        books_for_category = books_by_category[request.category]
-        num_results = min(request.max_results, len(books_for_category))
-        books_to_recommend = random.sample(books_for_category, num_results)
-
-        return RecommendationResponse(recommendations=books_to_recommend)
         
-    def GetBookById(self, request, context):
-        
-     
-    def GetBookByCategory(self, request, context):
+        books_with_category = books_database[request.category]
+        num_results = min(request.max_results, len(books_with_name))
+        searched_books = random.sample(books_with_category, num_results)
+
+        return RecommendationResponse(books=searched_books)
         
 
 
@@ -84,23 +56,11 @@ def serve():
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
     )
-    recommendations_pb2_grpc.add_RecommendationsServicer_to_server(
-        RecommendationService(), server
+    book_pb2_grpc.add_RecommendationsBookServicer_to_server(
+        BookService(), server
     )
-
-    with open("server.key", "rb") as fp:
-        server_key = fp.read()
-    with open("server.pem", "rb") as fp:
-        server_cert = fp.read()
-    with open("ca.pem", "rb") as fp:
-        ca_cert = fp.read()
-
-    creds = grpc.ssl_server_credentials(
-        [(server_key, server_cert)],
-        root_certificates=ca_cert,
-        require_client_auth=True,
-    )
-    server.add_secure_port("[::]:443", creds)
+    
+    server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
 
