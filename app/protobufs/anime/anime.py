326fcd2f60649a79908c3c2c63a1a2456abaa524
tiagorncarvalho <schizness@gmail.com>
2021-03-16 01:04:10 +0000

    Update anime.py
    Updated anime.py, not tested

--- app/protobufs/anime/anime.py
+++ app/protobufs/anime/anime.py
@@ -20,34 +20,67 @@ import anime_pb2_grpc
 
 class AnimeService(anime_pb2_grpc.AnimeServicer):
     def SearchById(self, request, context):
+        cur = con.cursor()
+        cur.execute('SELECT anime_id,anime_title,genres,anime_rating FROM anime_data WHERE anime_id=?', (request.anime_id,))
         
-        if request.anime_id not in anime_database:
+        anime = cur.fetchone()
+        print(anime)
+        if anime is None:
             raise NotFound("Id not found")
+        send = AnimeData(anime_id = anime[0], anime_title = anime[1], genres = anime[2], anime_rating = anime[3])
+        print(send)
+        return AnimeResponse(anime = send)
+        #if request.anime_id not in anime_database:
+        #    raise NotFound("Id not found")
 
-        anime_with_id = anime_database[request.anime_id]
-        return AnimeData(anime=anime_with_id)
+        #anime_with_id = anime_database[request.anime_id]
+        #return AnimeData(anime=anime_with_id)
         
     def SearchByName(self, request, context):
     
-        if request.name not in anime_database:
-            raise NotFound("Name not found")
+        cur = con.cursor()
+        cur.execute('SELECT anime_id,anime_title,genres,anime_rating FROM anime_data WHERE anime_title LIKE ? ', ( request.name,))
+        anime_with_name = cur.fetchall()
         
-        anime_with_name = anime_database[request.name]
-        num_results = min(request.max_results, len(anime_with_name))
-        searched_anime = random.sample(anime_with_name, num_results)
+        if anime_with_name is None:
+            raise NotFound("Id not found")
+            
+        num_results = min(request.max_results, len(animes_with_name))
+        searched_animes = random.sample(animes_with_name, num_results)
 
-        return AnimeDataList(anime=searched_anime)
+        return AnimeDataList(anime=searched_animes)
+    
+        #if request.name not in anime_database:
+        #    raise NotFound("Name not found")
+        
+        #anime_with_name = anime_database[request.name]
+        #num_results = min(request.max_results, len(anime_with_name))
+        #searched_anime = random.sample(anime_with_name, num_results)
+
+        #return AnimeDataList(anime=searched_anime)
         
     def SearchByCategory(self, request, context):
     
-        if request.category not in anime_database:
-            raise NotFound("Category not found")
+        cur = con.cursor()
+        cur.execute('SELECT anime_id,anime_title,genres,anime_rating FROM anime_data WHERE genres LIKE ? ', ("%" + request.category + "%",))
+        anime_with_category = cur.fetchall()
         
-        anime_with_category = anime_database[request.category]
-        num_results = min(request.max_results, len(anime_with_name))
-        searched_anime = random.sample(anime_with_category, num_results)
+        if anime_with_category is None:
+            raise NotFound("Id not found")
+            
+        num_results = min(request.max_results, len(anime_with_category))
+        searched_animes = random.sample(animes_with_category, num_results)
 
-        return AnimeDataList(anime=searched_anime)
+        return AnimeDataList(animes=searched_animes)
+    
+        #if request.category not in anime_database:
+        #    raise NotFound("Category not found")
+        
+        #anime_with_category = anime_database[request.category]
+        #num_results = min(request.max_results, len(anime_with_name))
+        #searched_anime = random.sample(anime_with_category, num_results)
+
+        #return AnimeDataList(anime=searched_anime)
         
 
 
