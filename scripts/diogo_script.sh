7056c356567d1c13b171e9aff9ceb7fbb965bf28
Diogo Lopes <diogo.oliveira.lopes@gmail.com>
2021-04-06 14:55:55 +0100

    Added file to install all the requirements

---
+++ scripts/diogo_script.sh
@@ -0,0 +1,20 @@
+echo "Installing requirements for anime..."
+pip install -r ./app/protobufs/anime/requirements.txt
+
+echo "Installing requirements for book..."
+pip install -r ./app/protobufs/book/requirements.txt
+
+echo "Installing requirements for imdb..."
+pip install -r ./app/protobufs/imdb/requirements.txt
+
+echo "Installing requirements for library..."
+pip install -r ./app/protobufs/library/requirements.txt
+
+echo "Installing requirements for api_gateway..."
+pip install -r ./app/protobufs/api_gateway/requirements.txt
+
+echo "Installing requirements for account..."
+pip install -r ./app/protobufs/account/requirements.txt
+
+echo "Installing requirements for signin..."
+pip install -r ./app/protobufs/signin/requirements.txt
