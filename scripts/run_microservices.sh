6b579460aa502ac59c43847fa4daaa5521afcf48
Diogo Lopes <diogo.oliveira.lopes@gmail.com>
2021-04-06 16:57:11 +0100

    Created scripts for running microservices and for killing them

--- scripts/run_microservices.sh
+++
@@ -1,7 +0,0 @@
-python3 ./app/protobufs/anime/anime.py &
-python3 ./app/protobufs/book/book.py &
-python3 ./app/protobufs/imdb/imdb.py &
-python3 ./app/protobufs/library/library.py &
-python3 ./app/protobufs/account/account.py &
-python3 ./app/protobufs/signin/signin.py &
-python3 ./app/protobufs/api_gateway/api_gateway.py
