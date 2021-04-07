cdecfaf0a1898d2dfb2265ca9fec1856f41c3257
JotaFilip <joao.roq@hotmail.com>
2021-04-07 23:20:23 +0100

    now IMDB goes through both databases

--- app/protobufs/imdb/imdb.py
+++ app/protobufs/imdb/imdb.py
@@ -13,9 +13,13 @@ user = "seen"
 password = "ifFvApoasv9lLvqR"
 up = user + ":" + password
 
-client = MongoClient("mongodb+srv://"+up+"@movies.oysuj.mongodb.net/Movies?retryWrites=true&w=majority")
-db = client["database"]
-db = db["movies"]
+client1 = MongoClient("mongodb+srv://"+up+"@movies.oysuj.mongodb.net/Movies?retryWrites=true&w=majority")
+db1 = client1["database"]
+db1 = db1["movies"]
+
+client2 = MongoClient("mongodb+srv://"+up+"@movies-2.nlmxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db2 = client2["database"]
+db2 = db2["movies"]
 
 from imdb_pb2 import (
     IMDBData,
@@ -31,35 +35,85 @@ import imdb_pb2_grpc
 class IMDBService(imdb_pb2_grpc.IMDBServicer):
 
     def GetIMDBs(self, request, context):
-        page = request.page * request.max_results
-        results = list(db.find().skip(page).limit(request.max_results))
-        resultss = [ imdb_to_proto(imdb) for imdb in results ]
-        return IMDBDataList( imdbs = resultss )
+
+        n_results = request.max_results
+
+        db1_size = db1.estimated_document_count()
+        first = request.page * n_results
+        last = (request.page + 1) * request.max_results - 1
+
+        results = []
+        if last < db1_size:
+            results = list(db1.find().skip(first).limit(n_results))
+        elif first > db1_size:
+            results = list(db2.find().skip(first+1-db1_size).limit(n_results))
+        else:
+            limit1 = db1_size - first
+            limit2 = n_results - limit1
+            results  = list(db1.find().skip(first).limit(limit1))
+            results += list(db2.find().skip(db1_size-first+1).limit(limit2))
+
+        results = [ imdb_to_proto(imdb) for imdb in results ]
+        return IMDBDataList( imdbs = results )
         
     def SearchById(self, request, context):
-        results = list(db.find({ "_id": ObjectId(request.imdb_id) }).limit(1))
+        
+        results = list(db1.find({ "_id": ObjectId(request.imdb_id) }).limit(1))
+        if len(results) <= 0:
+            results = list(db2.find({ "_id": ObjectId(request.imdb_id) }).limit(1))
 
         if len(results) <= 0:
             raise NotFound("Id not found")
+
         return imdb_to_proto(results[0])
 
     def SearchByName(self, request, context):
-        results = list(db.find({ "name": request.name}).limit(request.max_results))
-        results = [ imdb_to_proto(imdb) for imdb in results ]
+        results = search_by_name(request.name,request.max_results)
         return IMDBDataList( imdbs = results )
 
     def SearchByCategory(self, request, context):
-        results = list(db.find({ "category": { "$all": [request.category] } }).limit(request.max_results))
+        n_results = request.max_results
+        results = list(db1.find({ "category": { "$all": [request.category] } }).limit(n_results))
+
+        if len(results) < n_results:
+            results += list(db2.find({ "category": { "$all": [request.category] } }).limit(n_results-len(results)))
+
         results = [ imdb_to_proto(imdb) for imdb in results ]
         return IMDBDataList( imdbs = results )
 
     def AddIMDB(self, request, context):
-        db.insert_one(proto_to_imdb(request))
+
+        request = search_by_name(request.name,1)
+        if SearchByName(request,None) != []:
+            # Cannot add IMDB
+            return Success(sucess=False)
+
+        imdb = proto_to_imdb(request)
+        try
+            db1.insert_one(imdb)
+        except:
+            try:
+                db2.insert_one(imdb)
+            except: 
+                return Sucess(success=False) 
         return Success(success=True)
 
     def RemoveIMDB(self, request, context):
-        db.remove(ObjectId(request.imdb_id))
+        try
+            db1.remove_one(ObjectId(request.imdb_id))
+        except:
+            try:
+                db2.insert_one(ObjectId(request.imdb_id))
+            except: 
+                return Sucess(success=False) 
         return Success(success=True)
+
+def search_by_name(name,n_results):
+    results = list(db1.find({ "name": request.name}).limit(n_results))
+    if len(results) < n_results:
+        results += list(db2.find({ "name": request.name}).limit(len(results)-n_results))
+
+    return [ imdb_to_proto(imdb) for imdb in results ]
 
 def imdb_to_proto(result):
     if(result["rating"]== '-'):
