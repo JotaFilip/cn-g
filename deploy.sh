86ceda407045e090053a9da10b957c437c07215b
João Afonso <johnlandon1999@gmail.com>
2021-04-11 15:49:54 +0100

    Adicao dos scripts de deploy e create cluster

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+
+cd cn-g/
+cd app/
+cd protobufs/
+cd account/
+docker build -t gcr.io/${PROJECT_ID}/account:v2 .
+cd ..
+cd signin/
+docker build -t gcr.io/${PROJECT_ID}/signin:v2 .
+cd ..
+cd api_gateway/
+docker build -t gcr.io/${PROJECT_ID}/api_gateway:v2 .
+cd ../book/
+docker build -t gcr.io/${PROJECT_ID}/book:v2 .
+cd ../anime/
+docker build -t gcr.io/${PROJECT_ID}/anime:v2 .
+cd ../imdb/
+docker build -t gcr.io/${PROJECT_ID}/imdb:v2 .
+cd ../library/
+docker build -t gcr.io/${PROJECT_ID}/library:v2 .
+docker images
+gcloud services enable containerregistry.googleapis.com
+gcloud auth configure-docker
+docker push gcr.io/${PROJECT_ID}/imdb:v2
+docker push gcr.io/${PROJECT_ID}/library:v2
+docker push gcr.io/${PROJECT_ID}/anime:v2
+docker push gcr.io/${PROJECT_ID}/book:v2
+docker push gcr.io/${PROJECT_ID}/account:v2
+docker push gcr.io/${PROJECT_ID}/signin:v2
+docker push gcr.io/${PROJECT_ID}/api_gateway:v2
+docker images
+gcloud container images list
+kubectl get nodes
+gcloud container clusters get-credentials cluster-recommendations --zone europe-west4-a
+kubectl apply -f deployment.yaml 
