d08347d2f4537c54b5d07652ac210ff2a5547bab
tiagorncarvalho <schizness@gmail.com>
2021-04-14 04:51:22 +0100

    Changes by saldanha

--- app/protobufs/api_gateway/api_gateway_test.sh
+++ app/protobufs/api_gateway/api_gateway_test.sh
@@ -7,16 +7,16 @@ retries=10
 interval=1
 url="http://127.0.0.1:5000"
 
-#while getopts r:i:k:u: option
-#do
-#    case "${option}"
-#    in
-#        r) retries=$OPTARG;;
-#        i) interval=$OPTARG;;
-#        k) keyword=$OPTARG;;
-#        u) url=$OPTARG;
-#    esac
-#done
+while getopts r:i:k:u: option
+do
+    case "${option}"
+    in
+        r) retries=$OPTARG;;
+        i) interval=$OPTARG;;
+        k) keyword=$OPTARG;;
+        u) url=$OPTARG;;
+    esac
+done
 
 echo "START CONTENT TEST"
 echo "retries: "$retries
@@ -26,7 +26,7 @@ echo "url: "$url
 # Admin adicionar livro
 for i in $(seq 0 $retries); do
 
-    res_comandocriarlivro=$(curl -X POST -u admin:admin --header  'Content-Type: application/json' -d '{"category": [ { "name": "Test"  } ], "description": "string", "name": "Test", "photoUrl": "test", "rating": 4.55,"type": "BOOK"  }' 'https://localhost:5000/item')
+    res_comandocriarlivro=$(curl -k -X POST -u admin:admin --header  'Content-Type: application/json' -d '{"category": [ { "name": "Test"  } ], "description": "string", "name": "Test", "photoUrl": "test", "rating": 4.55,"type": "BOOK"  }' 'https://localhost:5000/item')
 
     res=$res_comandocriarlivro || res=""
 
@@ -42,7 +42,7 @@ done
 # Utilizador normal tenta adicionar um livro e nao consegue
 for i in $(seq 0 $retries); do
 
-    res_comandocriarlivro=$(curl -X POST -u saldanha:saldanha --header  'Content-Type: application/json' -d '{"category": [ { "name": "Test"  } ], "description": "string", "name": "Test", "photoUrl": "test", "rating": 4.55,"type": "BOOK"  }' 'https://localhost:5000/item')
+    res_comandocriarlivro=$(curl -k -X POST -u saldanha:saldanha --header  'Content-Type: application/json' -d '{"category": [ { "name": "Test"  } ], "description": "string", "name": "Test", "photoUrl": "test", "rating": 4.55,"type": "BOOK"  }' 'https://localhost:5000/item')
 
     res=$res_comandocriarlivro || res=""
 
