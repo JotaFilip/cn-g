ae2170c617214d29b95933c1b77368a4ce9c7f73
tiagorncarvalho <schizness@gmail.com>
2021-04-14 02:57:07 +0100

    Added keys

--- scripts/gen_certs.sh
+++ scripts/gen_certs.sh
@@ -4,21 +4,21 @@
 #openssl req -x509 -nodes -newkey rsa:4096 -keyout ca.key -out ca.pem -subj /O=me
 
 # Generate a private key and certificate signing request for the client and server
-#openssl req -nodes -newkey rsa:4096 -keyout account.key -out account.csr -subj /CN=account
-#openssl req -nodes -newkey rsa:4096 -keyout anime.key -out anime.csr -subj /CN=anime
-#openssl req -nodes -newkey rsa:4096 -keyout api_gateway.key -out api_gateway.csr -subj "/C=PT/ST=Aveiro/L=Anadia/O=Leitao, Inc./OU=IT/CN=api-gateway"
-#openssl req -nodes -newkey rsa:4096 -keyout book.key -out book.csr -subj /CN=book
-#openssl req -nodes -newkey rsa:4096 -keyout imdb.key -out imdb.csr -subj /CN=imdb
-#openssl req -nodes -newkey rsa:4096 -keyout library.key -out library.csr -subj /CN=library
-#openssl req -nodes -newkey rsa:4096 -keyout signin.key -out signin.csr -subj /CN=signin
+openssl req -nodes -newkey rsa:4096 -keyout account.key -out account.csr -subj /CN=account
+openssl req -nodes -newkey rsa:4096 -keyout anime.key -out anime.csr -subj /CN=anime
+openssl req -nodes -newkey rsa:4096 -keyout api_gateway.key -out api_gateway.csr -subj "/C=PT/ST=Aveiro/L=Anadia/O=Leitao, Inc./OU=IT/CN=api-gateway"
+openssl req -nodes -newkey rsa:4096 -keyout book.key -out book.csr -subj /CN=book
+openssl req -nodes -newkey rsa:4096 -keyout imdb.key -out imdb.csr -subj /CN=imdb
+openssl req -nodes -newkey rsa:4096 -keyout library.key -out library.csr -subj /CN=library
+openssl req -nodes -newkey rsa:4096 -keyout signin.key -out signin.csr -subj /CN=signin
 
-openssl req -nodes -newkey rsa:4096 -keyout account.key -out account.csr -subj /CN=localhost
-openssl req -nodes -newkey rsa:4096 -keyout anime.key -out anime.csr -subj /CN=localhost
-openssl req -nodes -newkey rsa:4096 -keyout api_gateway.key -out api_gateway.csr -subj "/C=PT/ST=Aveiro/L=Anadia/O=Leitao, Inc./OU=IT/CN=localhost"
-openssl req -nodes -newkey rsa:4096 -keyout book.key -out book.csr -subj /CN=localhost
-openssl req -nodes -newkey rsa:4096 -keyout imdb.key -out imdb.csr -subj /CN=localhost
-openssl req -nodes -newkey rsa:4096 -keyout library.key -out library.csr -subj /CN=localhost
-openssl req -nodes -newkey rsa:4096 -keyout signin.key -out signin.csr -subj /CN=localhost
+#openssl req -nodes -newkey rsa:4096 -keyout account.key -out account.csr -subj /CN=localhost
+#openssl req -nodes -newkey rsa:4096 -keyout anime.key -out anime.csr -subj /CN=localhost
+#openssl req -nodes -newkey rsa:4096 -keyout api_gateway.key -out api_gateway.csr -subj "/C=PT/ST=Aveiro/L=Anadia/O=Leitao, Inc./OU=IT/CN=localhost"
+#openssl req -nodes -newkey rsa:4096 -keyout book.key -out book.csr -subj /CN=localhost
+#openssl req -nodes -newkey rsa:4096 -keyout imdb.key -out imdb.csr -subj /CN=localhost
+#openssl req -nodes -newkey rsa:4096 -keyout library.key -out library.csr -subj /CN=localhost
+#openssl req -nodes -newkey rsa:4096 -keyout signin.key -out signin.csr -subj /CN=localhost
 
 # Sign the client and server certs with the CA cert
 openssl x509 -req -in account.csr -CA ca.pem -CAkey ca.key -set_serial 1 -out account.pem
