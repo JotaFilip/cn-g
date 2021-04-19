7d8e7216585ebd10145a67b35c7aab69faf781ae
tiagorncarvalho <56092356+tiagorncarvalho@users.noreply.github.com>
2021-04-19 16:23:05 +0100

    Fix locust.sh file format

--- locust.sh
+++ locust.sh
@@ -26,5 +26,3 @@ kubectl apply -f kubernetes-config/locust-master-controller.yaml
 kubectl apply -f kubernetes-config/locust-master-service.yaml
 kubectl apply -f kubernetes-config/locust-worker-controller.yaml
 
-
-
