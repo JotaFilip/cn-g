912316119226897cb652333628d2b96ec2f2df6a
tiagorncarvalho <56092356+tiagorncarvalho@users.noreply.github.com>
2021-04-19 15:02:14 +0100

    Maybe? x4

--- install_istio.sh
+++ install_istio.sh
@@ -7,7 +7,7 @@ pwd
 export PATH="$PATH:/tmp/istio-1.9.3/bin"
 istioctl x precheck
 istioctl install
-kubectl get svc -n istio-system
-kubectl get pods -n istio-system
+kubectl get svc -n default
+kubectl get pods -n default
 kubectl label namespace default istio-injection=enabled
 kubectl create -n default secret tls istio-ingressgateway-certs --key ../cn-g/scripts/privkey1.pem --cert ../cn-g/scripts/fullchain1.pem
