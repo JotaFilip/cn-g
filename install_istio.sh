3e8131af5eef5a32a34380813615623ea3b5112a
Saldanha <miguelsaldanhafernandes@protonmail.com>
2021-04-19 13:53:04 +0100

    ver istio a dar

---
+++ install_istio.sh
@@ -0,0 +1,11 @@
+gcloud config set compute/zone europe-west4-a
+gcloud services enable container.googleapis.com
+curl -L https://istio.io/downloadIstio | sh -
+cd istio-1.9.3/
+pwd
+export PATH="$PATH:/tmp/istio-1.9.3/bin"
+istioctl x precheck
+istioctl install
+kubectl get svc -n istio-system
+kubectl get pods -n istio-system
+kubectl label namespace default istio-injection=enabled
