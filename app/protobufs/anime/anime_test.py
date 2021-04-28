d2ded2ac66feb91c277d2c9de7274a75268e0f32
mSALDANHAf <miguelsaldanhafernandes@hotmail.com>
2021-04-28 02:51:10 +0000

    tls sql

--- app/protobufs/anime/anime_test.py
+++ app/protobufs/anime/anime_test.py
@@ -23,24 +23,28 @@ animes_client = AnimeStub(animes_channel)
 
 class AnimeTestCase(unittest.TestCase):
 
-    def test_get_anime(self):
-        animes_request = AnimeByIdRequest(anime_id="606e252aebddc73ebfb15500")
-        self.assertEqual(animes_client.SearchById(animes_request).anime_title, "Inu X Boku Secret Service")
 
-    def test_remove_anime(self):
-        animes_request = AnimeByIdRequest(anime_id="606e252aebddc73ebfb15500")
-        self.assertEqual(animes_client.RemoveAnime(animes_request).success, True)
+
 
     def test_add_anime(self):
-        animes_request = AnimeData(anime_title="Testa", genres=["Teste"], anime_rating = 4.55, img_url="teste.jpg")
-        self.assertEqual(animes_client.AddAnime(animes_request).success, True)
+
+        animes_request = AnimeData(anime_title="Testa", genres = ["T"], anime_rating = 4.55, img_url="teste.jpg")
+        id = animes_client.AddAnime(animes_request).anime_id
+        self.assertNotEqual(id, None)
+        animes_request = AnimeByIdRequest(anime_id=id)
+        self.assertEqual(animes_client.RemoveAnime(animes_request).success, True)
+
+    def test_get_anime(self):
+        animes_request = AnimeByIdRequest(anime_id="606e252aebddc73ebfb15542")
+        self.assertEqual(animes_client.SearchById(animes_request).anime_title, "Lupin III: Lupin Ikka Seizoroi")
 
     def test_anime_search_by_name(self):
         animes_request = AnimeByNameRequest(name="One Piece", max_results=32)
         self.assertEqual(animes_client.SearchByName(animes_request).animes[0].anime_id, "606e252aebddc73ebfb15522")
 
     def test_anime_search_by_category(self):
-        self.assertNotEqual(animes_client.SearchByCategory("Action").animes.size, 0)
+        animes_request = AnimeByCategoryRequest(category="Action", max_results=32)
+        self.assertNotEqual(animes_client.SearchByCategory(animes_request).animes.size, 0)
 
 if __name__ == '__main__':
     unittest.main()
