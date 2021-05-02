4a1b4c89a35a7296fafe1771f7a43ad5c759adb5
tiagorncarvalho <56092356+tiagorncarvalho@users.noreply.github.com>
2021-05-02 15:54:20 +0100

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,5 +1,5 @@
 gcloud services enable cloudapis.googleapis.com  container.googleapis.com containerregistry.googleapis.com
-gcloud container clusters create cluster-recommendations  --zone=europe-west4-a --cluster-version=latest --num-nodes=3 --machine-type=n1-standard-4  --enable-autoscaling --min-nodes=1 --max-nodes=5   --enable-autorepair   --scopes=service-control,service-management,compute-rw,storage-ro,cloud-platform,logging-write,monitoring-write
+gcloud container clusters create cluster-recommendations  --zone=europe-west4-a --cluster-version=latest --num-nodes=3 --machine-type=n1-standard-1  --enable-autoscaling --min-nodes=1 --max-nodes=5   --enable-autorepair   --scopes=service-control,service-management,compute-rw,storage-ro,cloud-platform,logging-write,monitoring-write
 gcloud container clusters get-credentials cluster-recommendations --zone europe-west4-a
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value core/account)
 cd cn-g
