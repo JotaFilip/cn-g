6abb163de9693075503eeb59811d695f8dab5c95
JotaFilip <joao.roq@hotmail.com>
2021-05-13 17:12:38 +0100

    yaml update

--- app/protobufs/api_gateway/api_spark.py
+++ app/protobufs/api_gateway/api_spark.py
@@ -1,36 +1,6 @@
 
 
-########## Aux ##########
-types = {
-    "BOOK":  0,
-    "SHOW":  1,
-    "ANIME": 2,
-}
-ids = {
-    0: "BOOK",
-    1: "SHOW",
-    2: "ANIME",
-}
-def get_type(type):
-    if type not in types:
-        return None
-    return types[type]
-def get_id(id):
-    if id not in ids:
-        return None
-    return ids[id]
-#########################
 
-def getViewsOf(type,itemId):
-    type = get_type(type)
-    if not type: return 'false', 400
-
+def director(name):
     # TODO
-    return 0
-
-def getLikesOf(type,itemId):
-    type = get_type(type)
-    if not type: return 'false', 400
-
-    # TODO
-    return 0
+    pass
