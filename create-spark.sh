3c9dfe3be661577f6ae81130c59df4ba9ee3e58c
mSALDANHAf <miguelsaldanhafernandes@hotmail.com>
2021-05-21 13:08:20 +0100

    Spark

---
+++ create-spark.sh
@@ -0,0 +1,1 @@
+gcloud beta dataproc clusters create cluster-spark --region europe-west4 --zone europe-west4-a --master-machine-type n1-standard-4 --master-boot-disk-size 100 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 50 --image-version 2.0-debian10 --project cn-g14-projecto
